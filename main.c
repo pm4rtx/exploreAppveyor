a9299ffaa892ca384c7f06aeb0b08eb62813d4b0
pm4rtx <pm4rtx@gmail.com>
2021-08-16 02:19:49 +0900

    Conditionally include <stdint.h>

--- main.c
+++ main.c
@@ -92,7 +92,22 @@
 #endif
 
 #include <stddef.h>
-#include <stdint.h>
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1600)
+#   include <stdint.h>
+#else
+    typedef signed char int8_t;
+    typedef unsigned char uint8_t;
+
+    typedef signed short int int16_t;
+    typedef unsigned short int uint16_t;
+
+    typedef signed int int32_t;
+    typedef unsigned int uint32_t;
+
+    typedef signed __int64 int64_t;
+    typedef unsigned __int64 uint64_t;
+#endif
 
 int main(void)
 {
